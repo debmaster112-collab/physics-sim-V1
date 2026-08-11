e57d4240f7b37c6b32ae1c558450c3567067acc9
OHelloTheyr <debmaster112@gmail.com>
2026-08-11 20:00:47 +0300

    added 2D movement, bounce and energy conservation math

--- physicsV1.py
+++ physicsV1.py
@@ -6,8 +6,12 @@ meters_per_pixel = 0.01
 x = 720 
 y = 480 
 
-allVelocity = []
-allposition = []
+allVelocityX = []
+allVelocityY = []
+allpositionX = []
+allpositionY = []
+allPotentialEnergy = []
+allKineticEnergy = []
 AllTime = []
 
 pygame.init()
@@ -15,13 +19,17 @@ screen = pygame.display.set_mode((x, y))
 clock = pygame.time.Clock()
 running = True
 
-vel = 0
-acc = 9.81
+vel_x = 0
+vel_y = 0
+gravity = -9.81
+acc_x = 0
+acc_y = gravity
+ballMass = 1.0
 time = 0
 
 circleSize = 0.25
 
-circle_x = 7.2 / 2
+circle_x = circleSize #7.2 / 2
 circle_y = 4.8 / 2
 
 dt = 0
@@ -34,20 +42,32 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-
-    vel += acc * dt
-    circle_y += vel * dt
     time += dt
 
-    allVelocity.append(vel)
-    allposition.append(circle_y)
+    #X position
+    vel_x += acc_x * dt
+    circle_x += vel_x * dt
+
+    #Y position
+    vel_y += gravity * dt
+    circle_y += vel_y * dt
+
+    potential_energy = ballMass * gravity * circle_y
+    kinetic_energy = 0.5 * ballMass * (vel_x ** 2 + vel_y ** 2)
+
+    allVelocityX.append(vel_x)
+    allVelocityY.append(vel_y)
+    allpositionX.append(circle_x)
+    allpositionY.append(circle_y)
+    allPotentialEnergy.append(potential_energy)
+    allKineticEnergy.append(kinetic_energy)
     AllTime.append(time)
 
-    # if circle_y >= y - circleSize:
-        # circle_y = y - circleSize
-        # vel = -vel
+    if circle_y <= circleSize:
+        circle_y = circleSize
+        vel_y = -vel_y
 
-    pygame.draw.circle(screen, "green", (circle_x / meters_per_pixel, circle_y / meters_per_pixel), circleSize / meters_per_pixel)
+    pygame.draw.circle(screen, "green", (circle_x / meters_per_pixel, y - circle_y / meters_per_pixel), circleSize / meters_per_pixel)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -59,18 +79,37 @@ while running:
 
 pygame.quit()
 
-
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(2, 3)
 fig.suptitle("Physics Simulation")
 
-axs[0].plot(AllTime, allVelocity)
-axs[0].set_title("Velocity vs Time")
-axs[0].set_xlabel("Time (s)")
-axs[0].set_ylabel("Velocity (m/s)")
+axs[0, 0].plot(AllTime, allpositionX)
+axs[0, 0].set_title("Position X vs Time")
+axs[0, 0].set_xlabel("Time (s)")
+axs[0, 0].set_ylabel("Position X (m)") 
 
-axs[1].plot(AllTime, allposition)
-axs[1].set_title("Position vs Time")
-axs[1].set_xlabel("Time (s)")
-axs[1].set_ylabel("Position (m)")
+axs[0, 1].plot(AllTime, allpositionY)
+axs[0, 1].set_title("Position Y vs Time")
+axs[0, 1].set_xlabel("Time (s)")
+axs[0, 1].set_ylabel("Position Y (m)") 
+
+axs[1, 0].plot(AllTime, allVelocityX)
+axs[1, 0].set_title("Velocity X vs Time")
+axs[1, 0].set_xlabel("Time (s)")
+axs[1, 0].set_ylabel("Velocity X (m/s)")
+
+axs[1, 1].plot(AllTime, allVelocityY)
+axs[1, 1].set_title("Velocity Y vs Time")
+axs[1, 1].set_xlabel("Time (s)")
+axs[1, 1].set_ylabel("Velocity Y (m/s)")
+
+axs[0, 2].plot(AllTime, allPotentialEnergy)
+axs[0, 2].set_title("Potential Energy vs Time")
+axs[0, 2].set_xlabel("Time (s)")
+axs[0, 2].set_ylabel("Potential Energy (J)")
+
+axs[1, 2].plot(AllTime, allKineticEnergy)
+axs[1, 2].set_title("Kinetic Energy vs Time")
+axs[1, 2].set_xlabel("Time (s)")
+axs[1, 2].set_ylabel("Kinetic Energy (J)")  
 
 plt.show()
